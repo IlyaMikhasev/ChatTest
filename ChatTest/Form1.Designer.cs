d1baeafaeab61b5b0beaa509f05c5f131d37d340
IlyaMikhasev <psfxslon@gmail.com>
2024-01-28 10:28:19 +0500

    Добавьте файлы проекта.

---
+++ ChatTest/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ChatTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortText = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.msgText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.IPText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PortText)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PortText
+            // 
+            this.PortText.Location = new System.Drawing.Point(179, 17);
+            this.PortText.Margin = new System.Windows.Forms.Padding(2);
+            this.PortText.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.PortText.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.PortText.Name = "PortText";
+            this.PortText.Size = new System.Drawing.Size(80, 20);
+            this.PortText.TabIndex = 0;
+            this.PortText.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 181);
+            this.textBox1.TabIndex = 3;
+            // 
+            // msgText
+            // 
+            this.msgText.Location = new System.Drawing.Point(22, 261);
+            this.msgText.Margin = new System.Windows.Forms.Padding(2);
+            this.msgText.Name = "msgText";
+            this.msgText.Size = new System.Drawing.Size(333, 20);
+            this.msgText.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 261);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 24);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // IPText
+            // 
+            this.IPText.Location = new System.Drawing.Point(22, 16);
+            this.IPText.Name = "IPText";
+            this.IPText.Size = new System.Drawing.Size(152, 20);
+            this.IPText.TabIndex = 6;
+            this.IPText.Text = "127.000.000.001";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.IPText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.msgText);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PortText);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PortText)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown PortText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox msgText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox IPText;
+    }
+}
+
---
+++ ChatTest/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ChatTest
+{
+    public partial class Form1 : Form
+    {
+        public Socket socket, ns;
+        public IPEndPoint ep;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public void MyAcceptCallbakFunction(IAsyncResult ia)
+        {   //получаем ссылку на слушающий сокет
+            socket = (Socket)ia.AsyncState;
+            //получаем сокет для обмена данными с клиентом
+            ns = socket.EndAccept(ia);
+
+            //создаем буфер и записываем в него сообщение получиенное от клиента
+            byte[] buf = new byte[1024];
+            int l = ns.Receive(buf);
+            Invoke((MethodInvoker)delegate
+            {
+                textBox1.Text += System.Text.Encoding.Unicode.GetString(buf, 0, l) + "\r\n";
+            });
+            ns.Send(buf, 0, buf.Length, SocketFlags.None);
+
+            //возобновляем асинхронный Accept
+            socket.BeginAccept(new AsyncCallback(MyAcceptCallbakFunction), socket);
+            ns.Shutdown(SocketShutdown.Both);
+            ns.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//подключение
+        {
+            if (socket != null)
+                return;
+            //создаем новый сокет 
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            //связываем сокет по нашему порту с ip адресом к которому могут подключаться с разных интерфесов
+            socket.Bind(new IPEndPoint(IPAddress.Any, (int)PortText.Value));
+            //устонавливаем прослушивание на 10 входящих вызовов(одновременно)
+            socket.Listen(10);
+            //начинаем ассинхронное прослушивание
+            socket.BeginAccept(new AsyncCallback(MyAcceptCallbakFunction), socket);
+        }
+
+        private void button2_Click(object sender, EventArgs e)// отправить сообщение
+        {
+            //создаем новый сокет
+            Socket ss = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            //создаем новый  endPoint с ip адрессом к которому хотим подключиться
+            ep = new IPEndPoint(IPAddress.Parse(IPText.Text), (int)PortText.Value);
+            if (!ss.Connected) ss.Connect(ep);
+            //кодируем и отправляем наше сообщение клиенту
+            ss.Send(System.Text.Encoding.Unicode.GetBytes(msgText.Text));
+            //выводим наше сообщение в textbox с пометкой отправленно
+            textBox1.Text += "=> " + msgText.Text + "\r\n";
+            //отключаемся от сервера
+            ss.Shutdown(SocketShutdown.Both);
+            ss.Close();
+        }
+    }
+}
